3a796eedf3533621826a9647c926940f206c36dd
Tryfyn <143002166+Tryfyn@users.noreply.github.com>
2024-02-06 11:08:24 -0500

    Update streamlit_app.py
    removed the normalization table

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,10 +16,6 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 #Let's put a pick a pick list here so they can pick the fruit that they want to include
 fruits_selected = streamlit.multiselect("Pick some Fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-#take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#output it to the screen as a table
-streamlit.dataframe(fruityvice_normalized)
 
                   
 
