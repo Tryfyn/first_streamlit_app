470bbd33a78e8c4950187e3337bea04d7186efa3
Tryfyn <143002166+Tryfyn@users.noreply.github.com>
2024-02-06 10:32:10 -0500

    Update streamlit_app.py
    Removed duplicate output line

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output it to the screen as a table
 streamlit.dataframe(fruityvice_normalized)
                      
-#output it the screen as a table 
-streamlit.dataframe(fruityvice_normalized)
 
