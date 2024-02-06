1e464d036653dbeaf8aa11dd21eb874f602e6894
Tryfyn <143002166+Tryfyn@users.noreply.github.com>
2024-02-06 11:04:44 -0500

    Update streamlit_app.py
    removed section for redo

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,28 +16,10 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 #Let's put a pick a pick list here so they can pick the fruit that they want to include
 fruits_selected = streamlit.multiselect("Pick some Fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 #take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output it to the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-
-#take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#output it to the screen as a table
-streamlit.dataframe(fruityvice_normalized)
-
-#New section to display fruityvice api response
-streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
-
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-
-                     
+                  
 
