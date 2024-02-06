4597eee115f587e1565942850a33873bc8a3ee3b
Tryfyn <143002166+Tryfyn@users.noreply.github.com>
2024-02-06 17:04:44 -0500

    Update streamlit_app.py
    replaced kiwi double quotes

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
 
 #Allow the user to add a fruit to a list
-add_my_fruit = streamlit.text_input('What fruit would you like to add?', "Kiwi')
+add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'Kiwi')
 streamlit.write('Thanks for adding', add_my_fruit)
 
 import requests 
