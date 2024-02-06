77c1ecc09d63cbb19a41c5781985274e64e0e899
Tryfyn <143002166+Tryfyn@users.noreply.github.com>
2024-02-06 16:49:48 -0500

    Update streamlit_app.py
    Add text box to add a fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,6 @@ my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
+
+add_my_fruit= streamlit.text('What fruit would you like to add?', "Kiwi')
+streamlit.write('The user entered' .fruit_choice)
