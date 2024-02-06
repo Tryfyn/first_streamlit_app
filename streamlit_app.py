b28b978a34f983dd66ee063542154c798c5807c4
Tryfyn <143002166+Tryfyn@users.noreply.github.com>
2024-02-06 16:52:25 -0500

    Update streamlit_app.py
    Added missing input text

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,5 +43,5 @@ my_data_row = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
 
-add_my_fruit= streamlit.text('What fruit would you like to add?', "Kiwi')
+add_my_fruit= streamlit.text_input('What fruit would you like to add?', "Kiwi')
 streamlit.write('The user entered' ,add_my_fruit)
